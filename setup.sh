9efe2a9ee27036d4f891d31ff7492fce6d29103f
Ville <58787815+uville@users.noreply.github.com>
2024-06-03 18:13:30 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,3 +5,4 @@ sudo apt install git
 sudo snap install --classic code
 sudo snap install bitwarden
 snap install spotify
+sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
