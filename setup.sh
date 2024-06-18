ea9c05e96306889a21faa744b2bdc8cc42fc8549
Ville <58787815+uville@users.noreply.github.com>
2024-06-18 21:48:36 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,3 +8,4 @@ sudo snap install spotify
 sudo apt install zsh
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 sudo apt install python3.12-venv
+sudo snap install steam
