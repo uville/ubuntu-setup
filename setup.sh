cec79cec1957b7143208c89b1210efc65863358d
Ville <58787815+uville@users.noreply.github.com>
2024-06-21 21:58:21 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,8 +7,8 @@ sudo snap install bitwarden
 sudo snap install spotify
 sudo apt install zsh
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
-sudo apt install python3.12-venv
 sudo apt install python3 python3-pip
+sudo apt install python3-venv -y
 sudo snap install steam
 sudo dpkg --add-architecture i386
 sudo apt update
