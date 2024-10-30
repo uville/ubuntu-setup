de04a92e586c6190bc714addfceb2f928c1122fb
Ville <58787815+uville@users.noreply.github.com>
2024-10-30 13:30:11 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,9 @@ sudo apt install git -y
 sudo apt install code -y
 sudo snap install bitwarden -y
 sudo apt install firefox -y
+sudo apt install chromium-browser -y
 sudo apt install zsh -y
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 sudo apt install python3 python3-pip -y
 sudo apt install python3-venv -y
+
