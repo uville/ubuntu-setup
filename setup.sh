25cdcc3dd499bb1d79faf1811e0a0e449e9597a1
Ville <58787815+uville@users.noreply.github.com>
2024-10-30 14:29:18 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,29 +1,87 @@
 #!/bin/bash
-sudo apt-get update -y
-sudo apt-get upgrade -y
-echo "Update succeeded"
-sudo apt install git -y
-echo "Git installation succeeded"
-sudo snap install code
-echo "VSCode installation succeeded"
-sudo snap install bitwarden
-echo "Bitwarden installation succeeded"
-sudo apt install firefox -y
-echo "Firefox installation succeeded"
-sudo apt install chromium-browser -y
-echo "Chromium-browser installation succeeded"
-sudo snap install discord
-echo "Discord installation succeeded"
-sudo snap install azuredatastudio
-echo "Azure Data Studio installation succeeded"
-sudo snap install storage-explorer
-echo "Azure Storage Explorer installation succeeded"
-sudo apt install python3 python3-pip -y
-echo "Python3 and pip installation succeeded"
-sudo apt install python3-venv -y
-echo "Python3 venv installation succeeded"
-sudo apt install zsh -y
-echo "Zsh installation succeeded"
-sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
-echo "OhMyZsh installation succeeded"
+if sudo apt-get update -y ; then
+    echo "apt-get update done"
+else
+    echo "apt-get update failed"
+fi
+
+if sudo apt-get upgrade -y ; then
+    echo "apt-get update done"
+else
+    echo "apt-get update failed"
+fi
+
+
+if sudo apt install git -y ; then
+    echo "Git installation done"
+else
+    echo "Git installation failed"
+fi
+
+if sudo snap install code ; then
+    echo "VSCode installation done"
+else
+    echo "VSCode installation failed"
+fi
+
+if sudo snap install bitwarden ; then
+    echo "Bitwarden installation done"
+else
+    echo "Bitwarden installation failed"
+fi
+
+if sudo apt install firefox -y ; then
+    echo "Firefox installation done"
+else
+    echo "Firefox installation failed"
+fi
+
+if sudo apt install chromium-browser -y ; then
+    echo "Chromium installation done"
+else
+    echo "Chromium installation failed"
+fi
+
+
+if sudo snap install discord ; then
+    echo "Discord installation done"
+else
+    echo "Discord installation failed"
+fi
+
+if sudo snap install azuredatastudio ; then
+    echo "Azure Data Studio installation done"
+else
+    echo "Azure Data Studio installation failed"
+fi
+
+if sudo snap install storage-explorer ; then
+    echo "Azure Storage Explorer installation done"
+else
+    echo "Azure Storage Explorer installation failed"
+fi
+
+if sudo apt install python3 python3-pip -y ; then
+    echo "Python3 & pip installation done"
+else
+    echo "Python3 & pip installation failed"
+fi
+
+if sudo apt install python3-venv -y ; then
+    echo "Python3-venv installation done"
+else
+    echo "Python3-venv installation failed"
+fi
+
+if sudo apt install zsh -y ; then
+    echo "Zsh installation done"
+else
+    echo "Zsh installation failed"
+fi
+
+if sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" ; then
+    echo "OhMyZsh installation done"
+else
+    echo "OhMyZsh installation failed"
+fi
 
