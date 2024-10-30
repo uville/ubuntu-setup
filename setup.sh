713fed50d83727eaecd6466be6f0888aef41fc2a
Ville <58787815+uville@users.noreply.github.com>
2024-10-30 14:19:30 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 sudo apt-get update -y
 sudo apt-get upgrade -y
+echo "Update succeeded"
 sudo apt install git -y
-sudo apt install code -y
-sudo snap install bitwarden -y
+echo "Git installation succeeded"
+sudo apt install code
+echo "VSCode installation succeeded"
+sudo snap install bitwarden
+echo "Bitwarden installation succeeded"
 sudo apt install firefox -y
+echo "Firefox installation succeeded"
 sudo apt install chromium-browser -y
-sudo snap install discord -y
-sudo snap install azuredatastudio -y
-sudo snap install storage-explorer -y
-sudo apt install zsh -y
-sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+echo "Chromium-browser installation succeeded"
+sudo snap install discord
+echo "Discord installation succeeded"
+sudo snap install azuredatastudio
+echo "Azure Data Studio installation succeeded"
+sudo snap install storage-explorer
+echo "Azure Storage Explorer installation succeeded"
 sudo apt install python3 python3-pip -y
+echo "Python3 and pip installation succeeded"
 sudo apt install python3-venv -y
+echo "Python3 venv installation succeeded"
+sudo apt install zsh -y
+echo "Zsh installation succeeded"
+sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+echo "OhMyZsh installation succeeded"
 
